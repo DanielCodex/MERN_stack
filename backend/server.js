ab2e60f29a62aad6c510638be34ffd4bdc835c49
DanielCodex <danialups1997@gmail.com>
2022-12-30 02:31:47 +0100

    done for tonight

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next()
+})
+
+app.get("/", (req, res) => {
+  res.json({msg: "hello there"});
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
